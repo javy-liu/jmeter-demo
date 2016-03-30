594679a33e729ee5068703aaded98efb9844dca2
liuzhenyuan <liuzhenyuan@meituan.com>
2016-03-30 17:18:25 +0800

    添加例子

--- src/test/java/org/jerry/jmeter/JmeterDemoTest.java
+++ src/test/java/org/jerry/jmeter/JmeterDemoTest.java
@@ -2,18 +2,34 @@ package org.jerry.jmeter;
 
 import org.junit.Test;
 
+import java.util.logging.Logger;
+
 /**
  * @author liuzhenyuan
  * @version Last modified 16/3/29
  */
 public class JmeterDemoTest {
+    private static Logger log = Logger.getLogger(JmeterDemoTest.class.getName());
 
     private int i = 0;
 
+//    public JmeterDemoTest() {
+//
+//    }
+//
+//    public JmeterDemoTest(String label) {
+//
+//    }
+
     @Test
     public void test01() throws Exception {
-        for (int j = 0; j < 100; j++) {
+        for (int j = 0; j < 10; j++) {
+            log.warning("=======================");
             System.out.println(i++);
+            System.out.println("===================");
+            Thread.sleep(100);
         }
     }
+
+
 }
